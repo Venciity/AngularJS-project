204244a9e2e5aab6fc06af514aa711ce3732d030
Ventsislav Yordanov <venciity@abv.bg>
2015-01-02 22:33:52 +0200

    update HomeController

--- Ads/Client/app/js/controllers/HomeController.js
+++ Ads/Client/app/js/controllers/HomeController.js
@@ -2,6 +2,15 @@
  * Created by Ventsislav on 2.1.2015 г..
  */
 
-adsApp.controller('HomeController', ['$scope', function($scope) {
-    // TODO: ADD FUNCTIONALITY
+adsApp.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+
+    $http.get('http://softuni-ads.azurewebsites.net/api/ads').
+        success(function(data) {
+            console.log(data);
+            $scope.ads = data['ads'];
+        }).
+        error(function() {
+            console.log('Error')
+        }
+    );
 }]);
