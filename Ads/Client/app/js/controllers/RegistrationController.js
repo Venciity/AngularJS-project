3e91967b4f756ebcf5c9115d0050cb7f0aa4aabf
Ventsislav Yordanov <venciity@abv.bg>
2015-01-03 20:36:48 +0200

    add automatically logged in after registration

--- Ads/Client/app/js/controllers/RegistrationController.js
+++ Ads/Client/app/js/controllers/RegistrationController.js
@@ -25,6 +25,24 @@ adsApp.controller('RegistrationController',['$scope', '$http', '$location', func
       })
           .success(function(data){
               console.log(data);
+
+              $http.post('http://softuni-ads.azurewebsites.net/api/user/login', {
+                  username: $scope.registrationUsername,
+                  password: $scope.registrationPassword
+              })
+                  .success(function(data){
+                      console.log(data);
+                      success('Success login');
+                      sessionStorage.accessToken = data.access_token;
+                      sessionStorage.username = data.username;
+                      $location.path('/');
+                  })
+                  .error(function(data){
+                      console.log(data);
+                      error('Login Error');
+                  })
+
+
               success('Success registration');
               $location.path('/login');
           })
