5d7d2cb27568c959d215f8ecba84e9ede97da0f6
Ventsislav Yordanov <venciity@abv.bg>
2015-01-03 12:42:29 +0200

    add categories & towns to home page

--- Ads/Client/app/js/controllers/HomeController.js
+++ Ads/Client/app/js/controllers/HomeController.js
@@ -4,13 +4,31 @@
 
 adsApp.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 
-    $http.get('http://softuni-ads.azurewebsites.net/api/ads').
-        success(function(data) {
-            console.log(data);
+    $http.get('http://softuni-ads.azurewebsites.net/api/ads')
+        .success(function(data) {
             $scope.ads = data['ads'];
-        }).
-        error(function() {
-            console.log('Error')
+        })
+        .error(function() {
+            console.log('Error when get ads')
         }
     );
+
+    $http.get('http://softuni-ads.azurewebsites.net/api/categories')
+        .success(function(data){
+            $scope.categories = data;
+        })
+        .error(function(){
+            console.log('Error when get categories');
+        }
+    );
+
+    $http.get('http://softuni-ads.azurewebsites.net/api/towns')
+        .success(function(data){
+            $scope.towns = data;
+        })
+        .error(function(){
+            console.log('Error when get towns');
+        }
+    );
+
 }]);
