5ec26d5c06708911839ba15185c73c920f32fa45
Ventsislav Yordanov <venciity@abv.bg>
2015-01-05 20:52:34 +0200

    add filtering by status in my ads view

--- Ads/Client/app/js/controllers/MyAdsController.js
+++ Ads/Client/app/js/controllers/MyAdsController.js
@@ -48,10 +48,50 @@ adsApp.controller('MyAdsController', ['$scope', '$http', '$location', function($
                 error('Error load my ads');
             }
         );
-
     };
 
     $scope.loadMyAds();
+
+    $scope.loadMyAdsByStatus = function(status){
+        var request = {
+            method: 'GET',
+            url: 'http://softuni-ads.azurewebsites.net/api/user/ads?status=' + status,
+            headers: {
+                'Authorization': 'Bearer ' + sessionStorage.accessToken
+            },
+            data: {}
+        }
+
+        $http(request)
+            .success(function(data){
+                $scope.ads = data['ads'];
+                for (var ad in data['ads']){
+                    var currentAd  = data['ads'][ad];
+                    if (currentAd.status === 'WaitingApproval' || currentAd.status === 'Published'){
+                        currentAd.firstButtonClass = 'btn btn-warning';
+                        currentAd.firstButtonText = 'Deactivate';
+                        currentAd.secondButtonClass = 'hidden';
+                        currentAd.thirdButtonClass = 'hidden';
+                        currentAd.fourthButtonClass = 'hidden';
+                    }
+                    else if (currentAd.status === 'Inactive'){
+                        currentAd.firstButtonClass = 'hidden';
+                        currentAd.secondButtonClass = 'btn btn-default';
+                        currentAd.secondButtonText = 'Edit';
+                        currentAd.thirdButtonClass = 'btn btn-success';
+                        currentAd.thirdButtonText = 'Publish Again';
+                        currentAd.fourthButtonClass = 'btn btn-danger';
+                        currentAd.fourthButtonText = 'Delete';
+                    }
+                }
+            })
+            .error(function(data){
+                console.log(data);
+                error('Error load my ads by status');
+            }
+        );
+
+    };
 
     $scope.deactivateAd = function(id){
         var request = {
